ec9755838a8cf58f46604350d380360e338a9db1
Zak Kent <zak33@cornell.edu>
2022-08-17 16:28:19 -0400

    change term to node

--- src/Inference.hs
+++ src/Inference.hs
@@ -2,11 +2,11 @@
 
 module Inference (Error (..), decorate) where
 
-import Inference.Term (Term, Typing (..))
+import Inference.Term (Node, Typing (..))
 
 -- | An error that can arise from a type inference failure
 data Error = Error
 
 -- | Decorate each term in an AST with its inferred type, or produce an error
-decorate :: Term 'Untyped -> Either Error (Term 'Typed)
+decorate :: Node 'Untyped -> Either Error (Node 'Typed)
 decorate = undefined
